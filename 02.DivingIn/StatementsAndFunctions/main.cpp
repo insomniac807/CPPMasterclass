bf01b9d40c4d352b5808f56e861b3d8f18917e01
insomniac807 <17027013+insomniac807@users.noreply.github.com>
2024-01-02 22:16:30 +0000

    finishing up statements and functions example code

--- 02.DivingIn/StatementsAndFunctions/main.cpp
+++ 02.DivingIn/StatementsAndFunctions/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 
+int addNumbers(int num1, int num2)
+{
+    return num1 + num2;
+}
+
 int main(int argc, char **argv)
 {
     int firstNumber = 12;
     int secondNumber = 9;
 
-    int sum = firstNumber + secondNumber;
+    int sum = firstNumber + secondNumber; 
 
-    std::cout << "The sum of the two numbers is: " << sum << std::endl;
+    std::cout << "First Number: " << firstNumber << std::endl;
+    std::cout << "Second Number: " << secondNumber << std::endl;
+
+    std::cout << "The sum of the two numbers is: " << sum << std::endl; // sum is 21
+
+    sum = addNumbers(30, 21);
+    std::cout << "Sum is now : " << sum << std::endl;
+
+    std::cout << "Sum is now : " << addNumbers(25,50) << std::endl;
     
     return 0;
 }
