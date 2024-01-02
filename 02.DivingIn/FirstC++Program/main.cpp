6fe515de6a8e4ebf779a6558041bbd1e51653efa
insomniac807 <17027013+insomniac807@users.noreply.github.com>
2024-01-02 08:19:54 +0000

    cleaned up comments

--- 02.DivingIn/FirstC++Program/main.cpp
+++ 02.DivingIn/FirstC++Program/main.cpp
@@ -1,13 +1,15 @@
+/*
+    Simple Program. Prompts User to Enter Their name, stores as string in memory
+    and prints 10 times to the console.
+*/
+
 #include <iostream>
 //declare namespace to avoid typing std:: before every statement using the std library
 using namespace std;
 
 int main(){
-    //init string variable
     string name = "";
-    //prompt user for input
     cout << "Please Enter Your Name: ";
-    //store user input in string called name
     cin >> name;
     //print name 10 times to the console
     for(int i=0; i<10; i++)
@@ -15,5 +17,6 @@ int main(){
         cout << i+1 << ": ";
         cout << name << endl;
     }
-    return 0; // if return 0 program ran successfully
+    return 0;
+    //end program
 }
