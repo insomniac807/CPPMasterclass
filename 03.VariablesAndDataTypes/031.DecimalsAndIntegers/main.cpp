d6caf4b3a37c017b8e1641c35c950184e2127ce2
insomniac807 <17027013+insomniac807@users.noreply.github.com>
2024-01-03 23:50:34 +0000

    code clean up. sizeof() demo

--- 03.VariablesAndDataTypes/031.DecimalsAndIntegers/main.cpp
+++ 03.VariablesAndDataTypes/031.DecimalsAndIntegers/main.cpp
@@ -6,7 +6,7 @@ int main(){
     int dog_count{7}; // initalise int with value of 7
     int cat_count(10); //same as above with value of 10
     int domestic_animal_count {dog_count + cat_count}; // we can use operators to perform basic int calculation in init braces
-    // int doesnt_exist_count ( doesnt_exist1 + doesnt-exist2 ); // compiler error as variables have not been declared
+    // int doesnt_exist_count ( doesnt_exist1 + doesnt_exist2 ); // compiler error as variables have not been declared
     /*
         2.9 is of type double with larger range than int.ERROR or WARNING.
         If try to init in curley braces g++ compile failes
