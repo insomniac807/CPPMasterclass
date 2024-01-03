39cdec42ef26f4ccf9df1431227c9b9edeb26727
insomniac807 <17027013+insomniac807@users.noreply.github.com>
2024-01-03 23:21:38 +0000

    cleaned up some comments and code to match course demo

--- 03.VariablesAndDataTypes/031.DecimalsAndIntegers/main.cpp
+++ 03.VariablesAndDataTypes/031.DecimalsAndIntegers/main.cpp
@@ -9,8 +9,16 @@ int main(){
 
     int domestic_animal_count {dog_count + cat_count}; // we can use operators to perform basic int calculation in init braces
 
-    // int doesnt_exist_count { doesnt_exist1 + doesnt-exist2 } // compiler error as variables have not been declared
+    // int doesnt_exist_count ( doesnt_exist1 + doesnt-exist2 ); // compiler error as variables have not been declared
 
-    // int narrowing_conversion {2.9}; // 2.9 is of type double with larger range than int.ERROR or WARNING (won't compile)
+    /*
+        2.9 is of type double with larger range than int.ERROR or WARNING.
+        If try to init in curley braces g++ compile failes
+        If init in brackets C++ will truncate everything after the decimal point 
+            without performing a round up or down conversion
+        i.e. the value of narrowing_conversion below is 2
+    */
+    int narrowing_conversion (2.9); 
+    std::cout << narrowing_conversion << std::endl; // will print 2 to the console
     return 0;
 }
