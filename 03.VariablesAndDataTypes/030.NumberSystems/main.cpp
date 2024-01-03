4d8e394730c7a10db9f32979aae5e8a19a354d17
insomniac807 <17027013+insomniac807@users.noreply.github.com>
2024-01-03 22:46:58 +0000

    init commit, number systems demo

--- 03.VariablesAndDataTypes/030.NumberSystems/main.cpp
+++ 03.VariablesAndDataTypes/030.NumberSystems/main.cpp
@@ -8,7 +8,7 @@ int main(){
     int num1{15}; //decimal
     int num2{017}; // octal (notated by the leading 0)
     int num3{0x0f}; // hexadecimal (notated by the leading 0x)
-    int num4{0b1111}; // (binary (notated by the leading 0b)
+    int num4{0b1111}; // (binary (notated by the leading 0b) [only available for C++ 14 and later]
 
     std::cout << "Hello World, All numbers above should output as the value 15 to the console.." << std::endl;
     std::cout << num1 << std::endl;
