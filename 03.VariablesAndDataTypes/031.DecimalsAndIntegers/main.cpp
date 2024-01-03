9a740dcf790281cb4e439517c284a0c8bfd3d74f
insomniac807 <17027013+insomniac807@users.noreply.github.com>
2024-01-03 23:49:51 +0000

    code clean up. sizeof() demo

--- 03.VariablesAndDataTypes/031.DecimalsAndIntegers/main.cpp
+++ 03.VariablesAndDataTypes/031.DecimalsAndIntegers/main.cpp
@@ -3,14 +3,10 @@
 int main(){
     int elephant_count; // bad initalisation. can contain random garbage data. WARNING!
     int lion_count{}; // empty curley braces initalisation of integers will store default value of 0
-
     int dog_count{7}; // initalise int with value of 7
     int cat_count(10); //same as above with value of 10
-
     int domestic_animal_count {dog_count + cat_count}; // we can use operators to perform basic int calculation in init braces
-
     // int doesnt_exist_count ( doesnt_exist1 + doesnt-exist2 ); // compiler error as variables have not been declared
-
     /*
         2.9 is of type double with larger range than int.ERROR or WARNING.
         If try to init in curley braces g++ compile failes
@@ -34,6 +30,5 @@ int main(){
     */
     std::cout << sizeof(int) << std::endl; // prints 4 as int is 4 bytes of memory
     std::cout << sizeof(elephant_count) << std::endl; // prints 4 as elephant_count is an int
-
     return 0;
 }
