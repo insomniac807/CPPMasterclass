0d7a582a440227848600620292b6d752c21a3fba
insomniac807 <17027013+insomniac807@users.noreply.github.com>
2024-01-04 09:49:27 +0000

    basic BODMAS demo

--- 04.OperationsOnData/040.BasicOperations/main.cpp
+++ 04.OperationsOnData/040.BasicOperations/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main(){
+
+    /* ########## Basic Integer Arithmetic ########## */
     int number1 {2};
     int number2 {7};
     std::cout << "--------------------" << std::endl;
@@ -20,5 +22,18 @@ int main(){
     result = number2 % number1;
     std::cout << "result : " << result << std::endl; // prints 1
     std::cout << "--------------------" << std::endl;
+
+    /* ########## Precedence & Associations ########## */
+    std::cout << "--------------------" << std::endl;
+    int a {6};
+    int b {3};
+    int c {8};
+    int d {9};
+    int e {3};
+    int f {2};
+    int g {5};
+
+    result = a + b * c -d/e -f + g;
+    std::cout << "a + b * c -d/e -f + g = " << result << std::endl;
     return 0;
 }
