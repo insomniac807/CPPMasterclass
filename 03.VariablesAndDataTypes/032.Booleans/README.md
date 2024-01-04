c1b54ccfb9b695fbd8f9d781bf26c3c5b714e0f0
insomniac807 <17027013+insomniac807@users.noreply.github.com>
2024-01-04 02:20:03 +0000

    init main function and readme

--- 03.VariablesAndDataTypes/032.Booleans/README.md
+++ 03.VariablesAndDataTypes/032.Booleans/README.md
@@ -1,1 +1,7 @@
 # Booleans
+
+- 1 of 2 states
+- Either true or false
+- Takes up 1 byte (8 bits) of memory
+- Wasteful as can be represented with a single bit but ig not to worry?
+- datatype keyword is "bool" and values are lowercase "true" or "false"
--- 03.VariablesAndDataTypes/032.Booleans/main.cpp
+++ 03.VariablesAndDataTypes/032.Booleans/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
 int main(){
+    bool red_light {true};
+    bool green_light {false};
+
+    if(red_light) {
+        std::cout << "Stop!!" << std::endl;
+    } else {
+        std::cout << "Keep Going!!!" << std::endl;
+    }
+    
     return 0;
 }
